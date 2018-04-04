4e0f44d164d211e8b7ce3de091c1468d86764cde
zateyev <zateyev@greet-go.com>
2018-04-04 19:04:35 +0600

    mini refactoring

--- prepare_input_data/src/kz/greetgo/credit_prediction/prepare_input_data/client_to_json/SelectorAsJson.java
+++ prepare_input_data/src/kz/greetgo/credit_prediction/prepare_input_data/client_to_json/SelectorAsJson.java
@@ -1,6 +1,9 @@
 package kz.greetgo.credit_prediction.prepare_input_data.client_to_json;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -27,12 +30,14 @@ public class SelectorAsJson implements AutoCloseable {
       "    (select array_to_json(array_agg(po)) FROM (select * from plan_oper_tmp where contract_id = credit_tmp.contract_id) po) as plan_oper,\n" +
       "    (select array_to_json(array_agg(ov)) FROM (select * from overdue where contract_id = credit_tmp.contract_id) ov) as overdue,\n" +
       "    (select array_to_json(array_agg(fo)) FROM (select * from fact_oper where contract_id = credit_tmp.contract_id) fo) as fact_oper,\n" +
-      "    (select array_to_json(array_agg(am)) FROM (select * from acc_move where contract_id = credit_tmp.contract_id) am) as acc_move\n" +
+      "    (select array_to_json(array_agg(am)) FROM (select * from acc_move where contract_id = credit_tmp.contract_id) am) as acc_move,\n" +
+      "    (select array_to_json(array_agg(co)) FROM (select * from collateral where contract_id = credit_tmp.contract_id) co) as acc_move\n" +
       "   from credit_tmp where client_id = client_tmp.client_id) \n" +
       "  r) as credit,\n" +
       "  \n" +
       "  (select array_to_json(array_agg(ph)) FROM (select * from phone_tmp where client_id = client_tmp.client_id) ph) as phone\n" +
       " from client_tmp) t")) {
+//      " from client_tmp LIMIT 15) t")) {
 
       try (ResultSet resultSet = preparedStatement.executeQuery()) {
         int i = 0;
@@ -42,7 +47,7 @@ public class SelectorAsJson implements AutoCloseable {
           file = new File(pathToSave + "client-" + i + ".json_row.txt");
           //noinspection ResultOfMethodCallIgnored
           file.getParentFile().mkdirs();
-          writer =  new PrintWriter(file, "UTF-8");
+          writer = new PrintWriter(file, "UTF-8");
           String json = resultSet.getString(1);
           writer.println(json);
           writer.close();
